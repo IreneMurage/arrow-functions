1ad6767209f82dcfeda4279336070f47aa879158
IreneMurage <muthonimurage361@gmail.com>
2025-06-16 13:55:23 +0300

    corrects tests on index.js using npm test

---
+++ index.js
@@ -0,0 +1,9 @@
+const divide = function(){
+    let num1 = 2000
+    let num2 = 100
+    const divides = num1 / num2
+    return divides;
+}
+const square =  x => x * x;
+
+ const add = (y,z) => y +  z;
